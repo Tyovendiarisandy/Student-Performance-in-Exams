2a2b2c569c2930f7854185b43a396ad4eaabb2a8
Tyovendi Arisandy <100063943+Tyovendiarisandy@users.noreply.github.com>
2023-07-11 01:04:11 +0700

    Add files via upload

---
+++ homepage_app.py
@@ -0,0 +1,67 @@
+import pandas as pd
+import numpy as np
+import streamlit as st
+import sklearn 
+import imblearn
+import pickle 
+from PIL import Image
+
+# load dataset
+df = pd.read_csv('clean_dataset.csv')
+
+# load model
+model = pickle.load(open('xgb_fix_tuned.pkl','rb'))
+    
+# create title (homepage)
+def main():
+    load_image = Image.open('./homepage.jpg')
+    st.image(load_image)
+    st.title('The Student Performance in Exams Prediction')
+    st.subheader('Please Input Your Data Below!')
+
+    # choose menu input - Selectbox
+    # st.sidebar.subheader('Select Your Input')
+    gender = st.selectbox('Select Your Gender!', df['gender'].unique())
+    race = st.selectbox('Select Your Race or Ethnicity!', df['race/ethnicity'].unique())
+    parental_education = st.selectbox("Select Your Parent's Level of Education!", df['parental_level_of_education'].unique())
+    lunch = st.selectbox('Select Your Lunch Type!', df['lunch'].unique())
+    preparation_course = st.selectbox('Select Your Status Test Preparation Course!', df['test_preparation_course'].unique())
+
+    # subtitle for symptoms
+    st.subheader('Select Your Exams Score!')
+    
+    # choose menu input - selectbox for symptoms
+    math = st.slider('What is Your Math Score?', min_value=0, max_value=100)
+    reading = st.slider('What is Your Reading Score?', min_value=0, max_value=100)
+    writing = st.slider('What is Your Writing Score?', min_value=0, max_value=100),
+
+    # prediction - button for predict
+    if st.button('Predict'):
+    # input the data in dataframe
+        input_data = pd.DataFrame({
+        'gender': [gender],
+        'race': [race],
+        'parental_education': [parental_education],
+        'lunch': [lunch],
+        'preparation_course': [preparation_course],
+        'math': [math],
+        'reading': [reading],
+        'writing': [writing]
+        })
+
+        # do predict with model
+        prediction = model.predict(input_data)
+
+        st.subheader('Prediction Result')
+        if prediction[0] == 0:
+            st.success("You Didn't Pass The SAT Test!")
+        else:
+            st.success("You Passed The SAT Test!")
+    
+    st.write('----')
+    st.write('''
+    Dashboard Created by [Tyovendi Arisandy](https://www.linkedin.com/in/tyovendiarisandy/)
+    ''')
+
+if __name__=='__main__':
+    main()
