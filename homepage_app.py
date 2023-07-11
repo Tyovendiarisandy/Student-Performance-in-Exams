042c04bdd2aa37ce4237aecc9c4bfb103d0f532f
Tyovendi Arisandy <100063943+Tyovendiarisandy@users.noreply.github.com>
2023-07-11 21:11:32 +0700

    Update homepage_app.py

--- homepage_app.py
+++ homepage_app.py
@@ -54,9 +54,9 @@ def main():
 
         st.subheader('Prediction Result')
         if prediction[0] == 0:
-            st.success("You Didn't Pass The SAT Test!")
+            st.markdown('<span style="color:red">You Didn\'t The SAT Test!</span>', unsafe_allow_html=True)
         else:
-            st.success("You Passed The SAT Test!")
+            st.markdown('<span style="color:green">You Passed The SAT Test!</span>', unsafe_allow_html=True)
     
     st.write('----')
     st.write('''
